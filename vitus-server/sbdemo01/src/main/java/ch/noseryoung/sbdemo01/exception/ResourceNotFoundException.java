a2ff9920f64ba43f3730b48208ca08a55c103e9f
Vitus-at-work <vitus.kaminski@noseryoung.ch>
2025-06-11 11:37:10 +0200

    add correct files

--- vitus-server/sbdemo01/src/main/java/ch/noseryoung/sbdemo01/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ch.noseryoung.sbdemo01.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
